03fc4c6b3486f815c3902b5edec69d8eb9bd9050
Fabian Aguirre <aguirrefabian.unne@gmail.com>
2023-04-20 10:52:49 -0300

    FAA - header and info-news changes

--- src/themes/riunne/app/home-page/carousel-home/carousel-home.component.ts
+++ src/themes/riunne/app/home-page/carousel-home/carousel-home.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
-import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
-import { NgIf } from '@angular/common';
 
 @Component({
     selector: 'ri-carousel-home',
-    imports: [NgbCarouselModule, NgIf],
     templateUrl: './carousel-home.component.html',
     styleUrls: ['./carousel-home.component.scss'],
 })
-export class CarouselHome extends BaseComponent {
+export class CarouselHome {
     images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
 }
