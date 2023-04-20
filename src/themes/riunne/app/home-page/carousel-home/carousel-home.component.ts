f6f258c226c4bc03e1a6284949721f0452b97b0b
Fabian Aguirre <aguirrefabian.unne@gmail.com>
2023-04-20 11:25:57 -0300

    FAA - header and info-news changes

--- src/themes/riunne/app/home-page/carousel-home/carousel-home.component.ts
+++ src/themes/riunne/app/home-page/carousel-home/carousel-home.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'ri-carousel-home',
     templateUrl: './carousel-home.html',
-    styleUrls: ['./carousel-home.scss'],
 })
 export class CarouselHome {
     images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
