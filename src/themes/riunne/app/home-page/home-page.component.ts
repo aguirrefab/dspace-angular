57010aa0d325e38abc6cd09518c20d56bf722b3e
Fabian Aguirre <aguirrefabian.unne@gmail.com>
2023-04-27 09:35:44 -0300

    changes

--- src/themes/riunne/app/home-page/home-page.component.ts
+++ src/themes/riunne/app/home-page/home-page.component.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { HomePageComponent as BaseComponent } from '../../../../app/home-page/home-page.component';
 
 @Component({
-  selector: 'ri-home-page',
+  selector: 'ds-home-page',
   // styleUrls: ['./home-page.component.scss'],
   styleUrls: ['../../../../app/home-page/home-page.component.scss'],
-  //templateUrl: './home-page.component.html'
+  // templateUrl: './home-page.component.html'
   templateUrl: '../../../../app/home-page/home-page.component.html'
 })
 export class HomePageComponent extends BaseComponent {
