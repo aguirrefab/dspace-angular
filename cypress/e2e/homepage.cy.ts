c68d45b2e6ba99d143296707c99ff937851735d0
Fabian Aguirre <aguirrefabian.unne@gmail.com>
2023-05-09 12:43:02 -0300

    FAA2 - modificaciones sobre riunne theme

--- cypress/e2e/homepage.cy.ts
+++ cypress/e2e/homepage.cy.ts
@@ -6,8 +6,8 @@ describe('Homepage', () => {
     cy.visit('/');
   });
 
-  it('should display translated title "DSpace Angular :: Home"', () => {
-    cy.title().should('eq', 'DSpace Angular :: Home');
+  it('should display translated title "Repositorio UNNE"', () => {
+    cy.title().should('eq', 'Repositorio UNNE');
   });
 
   it('should contain a news section', () => {
--- src/themes/riunne/app/navbar/navbar.component.ts
+++ src/themes/riunne/app/navbar/navbar.component.ts
@@ -6,11 +6,11 @@ import { slideMobileNav } from '../../../../app/shared/animations/slide';
  * Component representing the public navbar
  */
 @Component({
-  selector: 'ds-navbar',
-  // styleUrls: ['./navbar.component.scss'],
-  styleUrls: ['../../../../app/navbar/navbar.component.scss'],
-  // templateUrl: './navbar.component.html',
-  templateUrl: '../../../../app/navbar/navbar.component.html',
+  selector: 'ri-navbar',
+  styleUrls: ['./navbar.component.scss'],
+  //styleUrls: ['../../../../app/navbar/navbar.component.scss'],
+  templateUrl: './navbar.component.html',
+  //templateUrl: '../../../../app/navbar/navbar.component.html',
   animations: [slideMobileNav]
 })
 export class NavbarComponent extends BaseComponent {
